ba39387426006166ad1407e4b9ec7f88aa202224
LokeshPatil-er <lokeshprabhakarpatil@gmail.com>
2025-11-21 19:21:47 +0530

    day 4 notes add

--- LibraryManagementSystem/LibraryManagementSystem/obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
+++ LibraryManagementSystem/LibraryManagementSystem/obj/Debug/net8.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+049eb9907fd696f90ee61c15c86e268d07840202")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
